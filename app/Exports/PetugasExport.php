c91e3f703d5963331b7f7658f7d2883c32982588
gladmartin <gladmartin24@gmail.com>
2019-06-21 16:29:35 +0700

    config export petugas

--- app/Exports/PetugasExport.php
+++ app/Exports/PetugasExport.php
@@ -4,8 +4,12 @@ namespace App\Exports;
 
 use App\Petugas;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Contracts\View\View;
 
-class PetugasExport implements FromCollection
+class PetugasExport implements FromView
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +17,37 @@ class PetugasExport implements FromCollection
     public function collection()
     {
         return Petugas::all();
+    }
+
+    public function view(): View
+    {
+        return view('export.excel.petugas', [
+            'petugas' => Petugas::all()
+        ]);
+    }
+
+    /**
+    * @var Petugas $petugas
+    */
+    public function map($petugas): array
+    {
+        return [
+            $petugas->nama,
+            $petugas->jk,
+            $petugas->agama,
+            $petugas->user->email,
+            $petugas->alamat,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Nama',
+            'Jenis Kelamain',
+            'Agama',
+            'Email',
+            'Alamat',
+        ];
     }
 }
