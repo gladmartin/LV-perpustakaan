c456243166cb2263bb2de0d74f577dcec7142885
gladmartin <gladmartin24@gmail.com>
2019-06-22 10:38:04 +0700

    menaambahkan anggotaexport

---
+++ app/Exports/AnggotaExport.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Exports;
+
+use App\Anggota;
+use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Contracts\View\View;
+
+class AnggotaExport implements FromView
+{ 
+    public function view(): View
+    {
+        return view('export.excel.anggota', [
+            'anggota' => Anggota::all()
+        ]);
+    }
+
+    /**
+    * @var Anggota $anggota
+    */
+    public function map($anggota): array
+    {
+        return [
+            $anggota->nama,
+            $anggota->jk,
+            $anggota->agama,
+            $anggota->user->email,
+            $anggota->alamat,
+        ];
+    }
+}
