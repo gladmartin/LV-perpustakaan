8df496622e043475ab7758f5c50b98ecc5346775
gladmartin <gladmartin24@gmail.com>
2019-06-22 11:11:22 +0700

    merubah exportpetugas

--- app/Exports/PetugasExport.php
+++ app/Exports/PetugasExport.php
@@ -8,32 +8,10 @@ use Illuminate\Contracts\View\View;
 
 class PetugasExport implements FromView
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
-    {
-        return Petugas::all();
-    }
-
     public function view(): View
     {
         return view('export.excel.petugas', [
             'petugas' => Petugas::all()
         ]);
-    }
-
-    /**
-    * @var Petugas $petugas
-    */
-    public function map($petugas): array
-    {
-        return [
-            $petugas->nama,
-            $petugas->jk,
-            $petugas->agama,
-            $petugas->user->email,
-            $petugas->alamat,
-        ];
     }
 }
